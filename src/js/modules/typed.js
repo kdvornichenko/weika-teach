cc890545af57f0a64bd15c6fbc50e8af2042d59f
kdvornichenko <gglade999@gmail.com>
2022-04-26 19:56:04 +0600

    fix

---
+++ src/js/modules/typed.js
@@ -0,0 +1,7 @@
+export function typedAnim() {
+    var typed = new Typed('.type', {
+        strings: ["Learn English For IT", "Weika_teach"],
+        typeSpeed: 80,
+        smartBackspace: true
+      });
+}
