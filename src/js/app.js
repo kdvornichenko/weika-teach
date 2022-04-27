66585816d634bbbf83a89cd88f7afebc9e824188
kdvornichenko <gglade999@gmail.com>
2022-04-27 22:52:04 +0600

    Dist

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-import * as flsFunctions from "./modules/functions.js";
-import * as menuFunction from "./modules/menu.js";
-import * as slideOnScroll from "./modules/slide-on-scroll.js";
-import * as formContact from "./modules/form.js";
-
-flsFunctions.isWebp();
-menuFunction.menu();
-slideOnScroll.slide();
-formContact.formSender();
