f39accc54bf74654119fa341474452480867c78e
kdvornichenko <gglade999@gmail.com>
2022-04-27 18:09:05 +0600

    Form test

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import * as flsFunctions from "./modules/functions.js";
 import * as menuFunction from "./modules/menu.js";
 import * as slideOnScroll from "./modules/slide-on-scroll.js";
+import * as formContact from "./modules/form.js";
 
 flsFunctions.isWebp();
 menuFunction.menu();
 slideOnScroll.slide();
+formContact.formSender();
